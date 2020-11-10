6939b9505dcad6b3c600938acfaa0da14ac9fdeb
Tara Shirvaikar <tarashirvaikar2021@u.northwestern.edu>
2020-11-10 16:26:21 -0600

    made changes to sram

--- ce361_CPU_Verilog/lib/syncram.v
+++ ce361_CPU_Verilog/lib/syncram.v
@@ -33,45 +33,45 @@ module syncram(clk,cs,oe,we,addr,din,dout);
   
   // task to check whether bits are in hex  
   // don't need it now since fscanf gets hex value only and checks it.
-  task checkHex;
-    input [7:0] bits;
-    output integer hex;
-    begin
-    i = 0;
-    while (i<8) begin
-      case (bits[i])
-        1'h0 : hex = 1;
-        1'h1 : hex = 1;
-        1'h2 : hex = 1;
-        1'h3 : hex = 1;
-        1'h4 : hex = 1;
-        1'h5 : hex = 1;
-        1'h6 : hex = 1;
-        1'h7 : hex = 1;
-        1'h8 : hex = 1;
-        1'h9 : hex = 1;
-        1'ha : hex = 1;
-        1'hb : hex = 1;
-        1'hc : hex = 1;
-        1'hd : hex = 1;
-        1'he : hex = 1;
-        1'hf : hex = 1;
-        1'hA : hex = 1;
-        1'hB : hex = 1;
-        1'hC : hex = 1;
-        1'hD : hex = 1;
-        1'hE : hex = 1;
-        1'hF : hex = 1;
-        default : hex = 0;
-    endcase
-    if (hex==0) begin
-      $display("ERROR: Data %h is not in hex format: " , bits);
-      $finish;
-    end
-    i = i +1 ;
-    end
-   end
-  endtask
+//  task checkHex;
+//    input [7:0] bits;
+//    output integer hex;
+//    begin
+//    i = 0;
+//    while (i<8) begin
+//      case (bits[i])
+//        1'h0 : hex = 1;
+//        1'h1 : hex = 1;
+//        1'h2 : hex = 1;
+//        1'h3 : hex = 1;
+//        1'h4 : hex = 1;
+//        1'h5 : hex = 1;
+//        1'h6 : hex = 1;
+//        1'h7 : hex = 1;
+//        1'h8 : hex = 1;
+//        1'h9 : hex = 1;
+//        1'ha : hex = 1;
+//        1'hb : hex = 1;
+//        1'hc : hex = 1;
+//        1'hd : hex = 1;
+//        1'he : hex = 1;
+//        1'hf : hex = 1;
+//        1'hA : hex = 1;
+//        1'hB : hex = 1;
+//        1'hC : hex = 1;
+//        1'hD : hex = 1;
+//        1'hE : hex = 1;
+//        1'hF : hex = 1;
+//        default : hex = 0;
+//    endcase
+//    if (hex==0) begin
+//      $display("ERROR: Data %h is not in hex format: " , bits);
+//      $finish;
+//    end
+//    i = i +1 ;
+//    end
+//   end
+//  endtask
   
   
   task initiate;
