8402c04e0b83f60775865f8ee108ea9314114f71
Tara Shirvaikar <tarashirvaikar2021@u.northwestern.edu>
2020-11-12 16:55:05 -0600

    changes to pc logic

--- ce361_CPU_Verilog/lib/syncram.v
+++ ce361_CPU_Verilog/lib/syncram.v
@@ -1,6 +1,6 @@
 module syncram(clk,cs,oe,we,addr,din,dout);
   
-  parameter mem_file = "unsigned_sum.dat";
+  parameter mem_file;
   input clk;
   input cs;
   input oe;
@@ -8,7 +8,7 @@ module syncram(clk,cs,oe,we,addr,din,dout);
   input [31:0] addr;
   input [31:0] din;
   output reg [31:0] dout;
-  
+
   
   integer check_hex = 1;
   integer file;
@@ -33,45 +33,45 @@ module syncram(clk,cs,oe,we,addr,din,dout);
   
   // task to check whether bits are in hex  
   // don't need it now since fscanf gets hex value only and checks it.
-//  task checkHex;
-//    input [7:0] bits;
-//    output integer hex;
-//    begin
-//    i = 0;
-//    while (i<8) begin
-//      case (bits[i])
-//        1'h0 : hex = 1;
-//        1'h1 : hex = 1;
-//        1'h2 : hex = 1;
-//        1'h3 : hex = 1;
-//        1'h4 : hex = 1;
-//        1'h5 : hex = 1;
-//        1'h6 : hex = 1;
-//        1'h7 : hex = 1;
-//        1'h8 : hex = 1;
-//        1'h9 : hex = 1;
-//        1'ha : hex = 1;
-//        1'hb : hex = 1;
-//        1'hc : hex = 1;
-//        1'hd : hex = 1;
-//        1'he : hex = 1;
-//        1'hf : hex = 1;
-//        1'hA : hex = 1;
-//        1'hB : hex = 1;
-//        1'hC : hex = 1;
-//        1'hD : hex = 1;
-//        1'hE : hex = 1;
-//        1'hF : hex = 1;
-//        default : hex = 0;
-//    endcase
-//    if (hex==0) begin
-//      $display("ERROR: Data %h is not in hex format: " , bits);
-//      $finish;
-//    end
-//    i = i +1 ;
-//    end
-//   end
-//  endtask
+  task checkHex;
+    input [7:0] bits;
+    output integer hex;
+    begin
+    i = 0;
+    while (i<8) begin
+      case (bits[i])
+        1'h0 : hex = 1;
+        1'h1 : hex = 1;
+        1'h2 : hex = 1;
+        1'h3 : hex = 1;
+        1'h4 : hex = 1;
+        1'h5 : hex = 1;
+        1'h6 : hex = 1;
+        1'h7 : hex = 1;
+        1'h8 : hex = 1;
+        1'h9 : hex = 1;
+        1'ha : hex = 1;
+        1'hb : hex = 1;
+        1'hc : hex = 1;
+        1'hd : hex = 1;
+        1'he : hex = 1;
+        1'hf : hex = 1;
+        1'hA : hex = 1;
+        1'hB : hex = 1;
+        1'hC : hex = 1;
+        1'hD : hex = 1;
+        1'hE : hex = 1;
+        1'hF : hex = 1;
+        default : hex = 0;
+    endcase
+    if (hex==0) begin
+      $display("ERROR: Data %h is not in hex format: " , bits);
+      $finish;
+    end
+    i = i +1 ;
+    end
+   end
+  endtask
   
   
   task initiate;
--- mips_cpu.v
+++ mips_cpu.v
@@ -1,6 +1,6 @@
-module mips_cpu (clk, reset);
+module mips_cpu (clk, reset, en);
 
-input clk, reset;
+input clk, reset, en;
 wire [31:0] from_sign_extend, read_data_1, read_data_2, to_alu, alu_result, data, write_data, from_shift_mux, from_adder_pc, adder_result; 
 wire [31:0] instruction;
 wire [1:0] alu_op;
@@ -12,126 +12,52 @@ wire [31:0] pc, pc_from_mux;
 
 reg [31:0] pc_next, data_addr;
 
+wire en_inst_mem;
+
+parameter mem_file = "bills_branch.dat";
+
 //should initialize instruction and data memory
-//initial begin
-	//pc_next = 32'h00400020;
-///end 
-assign pc = pc_next;	
 
 
-sram instr_mem(.cs(1'b1),.oe(1'b1),.we(1'b0),.addr(pc),.din(),.dout(instruction)); //cs: low = memory selected || oe: low = read data outputted || we: high = allowed to write
 
-syncram data_mem(.clk(clk), .cs(1'b1),.oe(mem_read),.we(mem_write),.addr(data_addr),.din(read_data_2),.dout(data)); // control signal write enable 
+sram #(mem_file) instr_mem  (.cs(en),.oe(en_inst_mem),.we(1'b0),.addr(pc),.din(),.dout(instruction)); //cs: low = memory selected || oe: low = read data outputted || we: high = allowed to write
 
-control_mechanism control(.opcode(instruction[31:26]),
-								  .alu_op(alu_op), 
-								  .jump (jump),
-								  .reg_dst (reg_dst),
-								  .mem_to_reg (mem_to_reg),
-								  .branch (branch),
-								  .mem_read (mem_read),
-								  .mem_write (mem_write),
-								  .alu_src (alu_src),
-								  .reg_write(reg_write));
+syncram #(mem_file) data_mem(.clk(~clk), .cs(en),.oe(mem_read),.we(mem_write),.addr(alu_result),.din(read_data_2),.dout(data)); // control signal write enable 
+
+control_mechanism control(.opcode(instruction[31:26]),.alu_op(alu_op), .jump (jump),.reg_dst (reg_dst), .mem_to_reg (mem_to_reg), .branch (branch), .mem_read (mem_read),
+				.mem_write (mem_write), .alu_src (alu_src),.reg_write(reg_write));
 								  
-mux_5 mux_for_write_reg (.sel(reg_dst), 
-								 .src0(instruction[20:16]), 
-								 .src1(instruction[15:11]), 
-								 .z(reg_dst_num));//The register destination number for the Write register comes from the rt field (bits 20:16). Otherwise from rd field.
+mux_5 mux_for_write_reg (.sel(reg_dst), .src0(instruction[20:16]), .src1(instruction[15:11]), .z(reg_dst_num));//The register destination number for the Write register comes from the rt field (bits 20:16). Otherwise from rd field.
 								 
-reg_file register(.Ra(instruction[25:21]),
-								.Rb(instruction[20:16]), 
-								.Rw(reg_dst_num),
-								.busW(write_data),
-								.busA(read_data_1),
-								.busB(read_data_2),
-								.RegWr(reg_write),//write_enable
-								.clk(clk));
+reg_file register(.Ra(instruction[25:21]),.Rb(instruction[20:16]), .Rw(reg_dst_num),.busW(write_data),.busA(read_data_1), .busB(read_data_2),.RegWr(reg_write), .clk(clk));
 								
-extender sign_extend (.x(instruction[15:0]),
-										.z(from_sign_extend));
+extender sign_extend (.x(instruction[15:0]),.z(from_sign_extend));
 				
-mux_32 mux_before_alu (.sel(alu_src), 
-							  .src0(read_data_2), 
-							  .src1(from_sign_extend), 
-							  .z(to_alu));
+mux_32 mux_before_alu (.sel(alu_src),  .src0(read_data_2), .src1(from_sign_extend),  .z(to_alu));
 							  
 
-alu_control_block alu_ctrl (.func(instruction[5:0]), 
-								    .alu_op(alu_op), 
-									 .alu_ctrl(alu_control_sig));		
+alu_control_block alu_ctrl (.func(instruction[5:0]), .alu_op(alu_op),  .alu_ctrl(alu_control_sig));		
 			
 			
-ALU alu_block (.ctrl(alu_control_sig), 
-						  .A(read_data_1),
-						  .B(to_alu), 
-						  .shamt(),
-						  .cout(),
-						  .ovf(),
-						  .ze(zero_from_alu),
-						  .R(alu_result));
+ALU alu_block (.ctrl(alu_control_sig),  .A(read_data_1), .B(to_alu), .ze(zero_from_alu), .R(alu_result));
 						  
 						  				
-mux_32 mux_after_data_mem (.sel(mem_to_reg), //should be 1 initially
-							      .src0(alu_result), 
-									.src1(data), 
-									.z(write_data));
+mux_32 mux_after_data_mem (.sel(mem_to_reg),  .src0(alu_result), .src1(data), .z(write_data));
 
 									
-sll_32bit shift_2 (.x(from_sign_extend),
-					 .shift(32'b00000000000000000000000000000010), //shfting left by 2
-					 .z(from_shift_mux));
+//program counters
 
-					 
-adder_32 adder_pc(.a(pc), 
-						.b(32'd4), //adding 4 
-						.z(from_adder_pc));					 
-					 
-					 
-adder_32 adder_after_shift(.a(from_adder_pc), 
-									.b(from_shift_mux), 
-									.z(adder_result));
+adder_32 adder_pc(.a(pc), .b(32'd4), .z(from_adder_pc));	
+				 
+sll_32bit shift_2 (.x(from_sign_extend), .shift(32'b00000000000000000000000000000010), .z(from_shift_mux));
+					 					 
+adder_32 adder_after_shift(.a(from_adder_pc), .b(from_shift_mux), .z(adder_result));
 									
+and_gate gate1(.x(branch), .y(zero_from_alu), .z(from_nand));
+					 			 
+mux_32 mux_after_adder (.sel(from_nand), .src0(from_adder_pc), .src1(adder_result), .z(pc_from_mux));
 
-and_gate gate1(.x(branch), 
-					 .y(zero_from_alu),
-					 .z(from_nand));
-					 
-					 
-mux_32 mux_after_adder (.sel(from_nand), 
-							.src0(from_adder_pc), 
-							.src1(adder_result), 
-							.z(pc_from_mux));
-
-always @(posedge clk)
-    begin
-    	if (reset == 0) begin
-    		pc_next <= 32'h00400020;
-		//data_addr <= 32'h10000000;
-	end	
-    	else
-    	begin
-		pc_next <= pc_from_mux;
-		//data_addr <= alu_result;		
-    	end	
-    end
-always @(negedge clk)
-    begin
-    	if (reset == 0) begin
-    		
-		data_addr <= 32'h10000000;
-	end	
-    	else
-    	begin
-		
-		data_addr <= alu_result;		
-    	end	
-    end
-
-//mux_32 mux_pc (.sel(reset), 
-//			.src0(32'h00400020), 
-//			.src1(pc_from_mux), 
-//			.z(pc_next));
+pc program_counter(.clk(clk), .reset(reset), .en(en), .pc_next(pc_next), .pc(pc), .en_inst_mem(en_inst_mem));
 
 				
 endmodule
